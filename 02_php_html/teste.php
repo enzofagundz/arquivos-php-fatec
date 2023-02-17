b6312a966ee272eaf1dc7d34471f672e20842707
Enzo Fagundes <enzo.fagundes014@gmail.com>
2023-02-17 17:06:29 -0300

    Aulas 1 e 2

---
+++ 02_php_html/teste.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste Html/PHP</title>
+</head>
+<body>
+    <h1>
+        Exemplo de código HTML e PHP
+    </h1>
+
+    <?php
+        echo 'Exemplo de código';
+        echo '<h3>Usando tags HTML através do PHP</h3>';
+    ?>
+</body>
+</html>
